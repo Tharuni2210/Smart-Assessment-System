af29060defb9d26d884933c48b707d80d314f709
Tharuni2210 <pandrakola.tharuni2005@gmail.com>
2026-02-16 22:07:09 +0530

    Add files via upload

---
+++ dashboard/__pycache__/dashboard/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from quizzes.models import Category
+
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def dashboard(request):
+    from quizzes.models import Category
+    categories = Category.objects.all()
+    return render(request, 'dashboard.html', {'categories': categories})
