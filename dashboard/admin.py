42530d9576bc8841a47c329338989ce2258e2fda
Tharuni2210 <pandrakola.tharuni2005@gmail.com>
2026-02-17 19:48:59 +0530

    Remove merge conflict markers from admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 # Register your models here.
-=======
+
+
 from django.contrib import admin
 
 # Register your models here.
->>>>>>> a86c6a1 (1st commit)
+
