bda8eefa5134a80642aa0042125452e206432193
Tharuni2210 <pandrakola.tharuni2005@gmail.com>
2026-03-24 18:59:30 +0530

    Fix merge conflict in admin.py
    Removed merge conflict markers and cleaned up comments.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 # Register your models here (none for dashboard)
-=======
-
-from django.contrib import admin
-
-# Register your models here.
 
 
 from django.contrib import admin
 
 # Register your models here.
 
->>>>>>> 9ef381430e7e615d936ccef98338886139338377
+
+from django.contrib import admin
+
+# Register your models here.
+
+
