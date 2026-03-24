a18270ad0e0ced2e714985391f4b6b98d2b28f13
Tharuni2210 <pandrakola.tharuni2005@gmail.com>
2026-03-24 18:58:06 +0530

    Remove duplicate import statements in models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 from django.db import models
 # No models defined in dashboard app
-=======
 
 from django.db import models
 
@@ -12,4 +11,3 @@ from django.db import models
 
 # Create your models here.
 
->>>>>>> 9ef381430e7e615d936ccef98338886139338377
