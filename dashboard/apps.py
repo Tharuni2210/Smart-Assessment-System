801de44775ed875a71f40dd61aee23120a6649a0
Tharuni2210 <pandrakola.tharuni2005@gmail.com>
2026-03-24 18:57:11 +0530

    Fix merge conflict in dashboard/apps.py
    Removed merge conflict markers from apps.py.

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 from django.apps import AppConfig
 
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
-=======
+
 
 from django.apps import AppConfig
 
@@ -18,4 +18,4 @@ from django.apps import AppConfig
 class DashboardConfig(AppConfig):
     name = 'dashboard'
 
->>>>>>> 9ef381430e7e615d936ccef98338886139338377
+
